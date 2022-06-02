46f15b2506f4675d9136b5c522037b157b1cec32
vguenoff <vguenoff@gmail.com>
2022-06-02 17:39:23 +0300

    Finish fundamentals

--- src/exercise/07.js
+++ src/exercise/07.js
@@ -4,41 +4,46 @@
 import * as React from 'react'
 
 const allItems = [
-  {id: 'apple', value: '🍎 apple'},
-  {id: 'orange', value: '🍊 orange'},
-  {id: 'grape', value: '🍇 grape'},
-  {id: 'pear', value: '🍐 pear'},
+    { id: 'apple', value: '🍎 apple' },
+    { id: 'orange', value: '🍊 orange' },
+    { id: 'grape', value: '🍇 grape' },
+    { id: 'pear', value: '🍐 pear' },
 ]
 
 function App() {
-  const [items, setItems] = React.useState(allItems)
+    const [items, setItems] = React.useState(allItems)
 
-  function addItem() {
-    const itemIds = items.map(i => i.id)
-    setItems([...items, allItems.find(i => !itemIds.includes(i.id))])
-  }
+    function addItem() {
+        const itemIds = items.map(i => i.id)
+        setItems([...items, allItems.find(i => !itemIds.includes(i.id))])
+    }
 
-  function removeItem(item) {
-    setItems(items.filter(i => i.id !== item.id))
-  }
+    function removeItem(item) {
+        setItems(items.filter(i => i.id !== item.id))
+    }
 
-  return (
-    <div className="keys">
-      <button disabled={items.length >= allItems.length} onClick={addItem}>
-        add item
-      </button>
-      <ul style={{listStyle: 'none', paddingLeft: 0}}>
-        {items.map(item => (
-          // 🐨 add a key prop to the <li> below. Set it to item.id
-          <li>
-            <button onClick={() => removeItem(item)}>remove</button>{' '}
-            <label htmlFor={`${item.id}-input`}>{item.value}</label>{' '}
-            <input id={`${item.id}-input`} defaultValue={item.value} />
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
+    return (
+        <div className="keys">
+            <button
+                disabled={items.length >= allItems.length}
+                onClick={addItem}
+            >
+                add item
+            </button>
+            <ul style={{ listStyle: 'none', paddingLeft: 0 }}>
+                {items.map(item => (
+                    <li key={item.id}>
+                        <button onClick={() => removeItem(item)}>remove</button>{' '}
+                        <label htmlFor={`${item.id}-input`}>{item.value}</label>{' '}
+                        <input
+                            id={`${item.id}-input`}
+                            defaultValue={item.value}
+                        />
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default App
